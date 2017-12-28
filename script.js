6ab0b5d5599dc83bf0d66c2c9d94668fb2c6b419
BeomyeonAndrewKim <beom911@gmail.com>
2017-12-29 03:17:17 +0900

    중앙 대각선 버그 제외 로직 완성

--- script.js
+++ script.js
@@ -1,67 +1,119 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Game Ready');
     let count = 0;
-    let column;
-    let row;
-    let whitePo = [];
-    let blackPo = [];
+    function finish(){
+        alert('Game over');
+        document.querySelectorAll('.col').forEach(el=>{
+            el.classList.remove('white');
+            el.classList.remove('black');
+        })
+    }
     document.querySelectorAll('.col').forEach(el => {
         el.addEventListener('click', e => {
+            let curPo={ col:Number(Array.from(el.classList)[0].split('-')[1]), row:Number(Array.from(el.closest('.row').classList)[0].split('-')[1])};
+            let curCol=Array.from(el.classList)[0];
+         
             if (el.classList.length >= 3) {
                 return false;
             } //같은 위치에 놓을시 false
             count += 1;
             if (count % 2 !== 0) {
                 el.classList.add("white");
-                let curCol=Array.from(el.classList)[0];
                 for(let i=0;i<11;i++){
                     if(
                         Array.from(el.closest('.row').children).slice(i,i+5).filter(item=>item.classList.contains('white')).length===5 ||
                         Array.from(document.querySelectorAll(`.${curCol}`)).slice(i,i+5).filter(item=>item.classList.contains('white')).length===5
                     ){
-                        alert('Game over');
+                        finish();
                     }
                 }
-                // console.log(document.querySelectorAll(Array.from(el.classList)[0]));
-                // Array.from(el.closest('.row').classList)
-                // console.log(Array.from(document.querySelector('.row-1').children).filter(item => item.includes('white')));
-                // let curRow=[];
-                // curRow.push([Array.from(el.closest('.row').children).filter(item => item.classList.contains('white'))]);
-                // console.log(curRow);
-                // if (Array.from(el.closest('.row').children).filter(item => item.classList.contains('white')).length >= 5) {
-                //     return console.log(false);
-                // }
-                // let curPo=[Number(Array.from(el.classList)[0].split('-')[1]), Number(Array.from(el.closest('.row').classList)[0].split('-')[1])];
-                // whitePo.push(curPo);
-                // console.log(whitePo);
-                // console.log(whitePo[0][1] - whitePo[0][0]);
-                // console.log(whitePo.map(item => item[0] - item[1]));
+                let newArr=[];
+                leftCross=Math.abs(curPo.col-curPo.row);
+                for(let j=15;j>leftCross;j--){
+                    if(curPo.col-curPo.row>=0){
+                    newArr.push(Array.from(document.querySelector(`.row-${j-leftCross}`).children).find(item=>item.classList.contains(`col-${j}`)));
+                } else {
+                    newArr.push(Array.from(document.querySelector(`.row-${j}`).children).find(item=>item.classList.contains(`col-${j-leftCross}`)));
+                }          
+                }
+                if(newArr.length>=5){
+                    for(let j=0; j<11;j++){
+                        if(newArr.slice(j,j+5).filter(item=>item.classList.contains('white')).length===5){
+                        finish();
+                    } 
+                }
+            }
+                let newArr2=[];
+                rightCross=curPo.col+curPo.row;
+                console.log(rightCross);
+                if(curPo.col+curPo.row>=15){
+                for(let k=15; k>rightCross-15;k--){  
+                newArr2.push(Array.from(document.querySelector(`.row-${k}`).children).find(item=>item.classList.contains(`col-${rightCross-k}`)));
+                }    
+                newArr2.push(Array.from(document.querySelector(`.row-${rightCross-15}`).children).find(item=>item.classList.contains('col-15')));
+                } else{
+                for(let l=1; l<rightCross;l++){
+                newArr2.push(Array.from(document.querySelector(`.row-${l}`).children).find(item=>item.classList.contains(`col-${rightCross-l}`))); 
+                    }
+                }
+                if(newArr2.length>=5){
+                    for(let m=0; m<11;m++){
+                        if(newArr2.slice(m,m+5).filter(item=>item.classList.contains('white')).length===5){
+                        finish();
+                    }
+                }
+            }
             } else {
                 el.classList.add("black");
-                let curCol=Array.from(el.classList)[0];
                 for(let i=0;i<11;i++){
                     if(
                         Array.from(el.closest('.row').children).slice(i,i+5).filter(item=>item.classList.contains('black')).length===5 ||
                         Array.from(document.querySelectorAll(`.${curCol}`)).slice(i,i+5).filter(item=>item.classList.contains('black')).length===5
-                ){
-                        alert('Game over');
+                    ){
+                        finish();
                     }
                 }
-                // blackPo.push([Number(Array.from(el.classList)[0].split('-')[1]), Number(Array.from(el.closest('.row').classList)[0].split('-')[1])]);
-                // console.log(blackPo);
-                // blackPo.push([Array.from(el.classList)[0].split('-')[1], Array.from(el.closest('.row').classList)[0].split('-')[1]]);
-                // console.log(blackPo);
+                let newArr=[];
+                leftCross=Math.abs(curPo.col-curPo.row);
+                for(let j=15;j>leftCross;j--){
+                    if(curPo.col-curPo.row>=0){
+                    newArr.push(Array.from(document.querySelector(`.row-${j-leftCross}`).children).find(item=>item.classList.contains(`col-${j}`)));
+                } else {
+                    newArr.push(Array.from(document.querySelector(`.row-${j}`).children).find(item=>item.classList.contains(`col-${j-leftCross}`)));
+                }          
+                }
+                if(newArr.length>=5){
+                    for(let j=0; j<11;j++){
+                        if(newArr.slice(j,j+5).filter(item=>item.classList.contains('black')).length===5){
+                        finish();
+                    }
+                }
             }
-            // document.querySelectorAll('.row').forEach((item, index, array) => {
-            //         console.log(Array.from(item.children).findIndex(array => array.includes('white')));
-
-            //     })
+            let newArr2=[];
+            rightCross=curPo.col+curPo.row;
+            console.log(rightCross);
+            if(curPo.col+curPo.row>=15){
+            for(let k=15; k>rightCross-15;k--){  
+            newArr2.push(Array.from(document.querySelector(`.row-${k}`).children).find(item=>item.classList.contains(`col-${rightCross-k}`)));
+            }    
+            newArr2.push(Array.from(document.querySelector(`.row-${rightCross-15}`).children).find(item=>item.classList.contains('col-15')));
+            } else{
+            for(let l=1; l<rightCross;l++){
+            newArr2.push(Array.from(document.querySelector(`.row-${l}`).children).find(item=>item.classList.contains(`col-${rightCross-l}`))); 
+                }
+            }
+            if(newArr2.length>=5){
+                for(let m=0; m<11;m++){
+                    if(newArr2.slice(m,m+5).filter(item=>item.classList.contains('white')).length===5){
+                    finish();
+                }
+            }
+        }
+                
+            }
                 //count 변수 통해 번갈아가며 돌 놓기
-                // column = Array.from(el.classList)[0];
-                // row = Array.from(el.closest('.row').classList)[0];
-                // console.log(Array.from(el.classList)[0].split('-')[1], Array.from(el.closest('.row').classList)[0].split('-')[1]);
-                console.log(Array.from(el.classList));
-                console.log(Array.from(el.closest('.row').classList));
+                console.log(Array.from(el.classList)[0].split('-')[1], Array.from(el.closest('.row').classList)[0].split('-')[1]);
+             
         })
     })
 });
