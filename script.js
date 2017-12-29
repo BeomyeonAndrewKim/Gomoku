0f8eb5cb822e0a1f23a1dfa89c6970d6f8a6f9d9
BeomyeonAndrewKim <beom911@gmail.com>
2017-12-29 15:13:30 +0900

    게임 로직 완성

--- script.js
+++ script.js
@@ -1,119 +1,118 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Game Ready');
     let count = 0;
-    function finish(){
+
+    function finish() {
         alert('Game over');
-        document.querySelectorAll('.col').forEach(el=>{
+        document.querySelectorAll('.col').forEach(el => {
             el.classList.remove('white');
             el.classList.remove('black');
+            count = 0;
         })
     }
     document.querySelectorAll('.col').forEach(el => {
         el.addEventListener('click', e => {
-            let curPo={ col:Number(Array.from(el.classList)[0].split('-')[1]), row:Number(Array.from(el.closest('.row').classList)[0].split('-')[1])};
-            let curCol=Array.from(el.classList)[0];
-         
+            let curPo = { col: Number(Array.from(el.classList)[0].split('-')[1]), row: Number(Array.from(el.closest('.row').classList)[0].split('-')[1]) };
+            let curCol = Array.from(el.classList)[0];
+
             if (el.classList.length >= 3) {
                 return false;
             } //같은 위치에 놓을시 false
             count += 1;
             if (count % 2 !== 0) {
                 el.classList.add("white");
-                for(let i=0;i<11;i++){
-                    if(
-                        Array.from(el.closest('.row').children).slice(i,i+5).filter(item=>item.classList.contains('white')).length===5 ||
-                        Array.from(document.querySelectorAll(`.${curCol}`)).slice(i,i+5).filter(item=>item.classList.contains('white')).length===5
-                    ){
+                for (let i = 0; i < 11; i++) {
+                    if (
+                        Array.from(el.closest('.row').children).slice(i, i + 5).filter(item => item.classList.contains('white')).length === 5 ||
+                        Array.from(document.querySelectorAll(`.${curCol}`)).slice(i, i + 5).filter(item => item.classList.contains('white')).length === 5
+                    ) {
                         finish();
                     }
                 }
-                let newArr=[];
-                leftCross=Math.abs(curPo.col-curPo.row);
-                for(let j=15;j>leftCross;j--){
-                    if(curPo.col-curPo.row>=0){
-                    newArr.push(Array.from(document.querySelector(`.row-${j-leftCross}`).children).find(item=>item.classList.contains(`col-${j}`)));
+                let newArr = [];
+                leftCross = Math.abs(curPo.col - curPo.row);
+                for (let j = 15; j > leftCross; j--) {
+                    if (curPo.col - curPo.row >= 0) {
+                        newArr.push(Array.from(document.querySelector(`.row-${j-leftCross}`).children).find(item => item.classList.contains(`col-${j}`)));
+                    } else {
+                        newArr.push(Array.from(document.querySelector(`.row-${j}`).children).find(item => item.classList.contains(`col-${j-leftCross}`)));
+                    }
+                }
+                if (newArr.length >= 5) {
+                    for (let j = 0; j < 11; j++) {
+                        if (newArr.slice(j, j + 5).filter(item => item.classList.contains('white')).length === 5) {
+                            finish();
+                        }
+                    }
+                }
+                let newArr2 = [];
+                rightCross = curPo.col + curPo.row;
+                if (curPo.col + curPo.row > 15) {
+                    for (let k = 15; k > rightCross - 15; k--) {
+                        newArr2.push(Array.from(document.querySelector(`.row-${k}`).children).find(item => item.classList.contains(`col-${rightCross-k}`)));
+                    }
+                    newArr2.push(Array.from(document.querySelector(`.row-${rightCross-15}`).children).find(item => item.classList.contains('col-15')));
                 } else {
-                    newArr.push(Array.from(document.querySelector(`.row-${j}`).children).find(item=>item.classList.contains(`col-${j-leftCross}`)));
-                }          
-                }
-                if(newArr.length>=5){
-                    for(let j=0; j<11;j++){
-                        if(newArr.slice(j,j+5).filter(item=>item.classList.contains('white')).length===5){
-                        finish();
-                    } 
-                }
-            }
-                let newArr2=[];
-                rightCross=curPo.col+curPo.row;
-                console.log(rightCross);
-                if(curPo.col+curPo.row>=15){
-                for(let k=15; k>rightCross-15;k--){  
-                newArr2.push(Array.from(document.querySelector(`.row-${k}`).children).find(item=>item.classList.contains(`col-${rightCross-k}`)));
-                }    
-                newArr2.push(Array.from(document.querySelector(`.row-${rightCross-15}`).children).find(item=>item.classList.contains('col-15')));
-                } else{
-                for(let l=1; l<rightCross;l++){
-                newArr2.push(Array.from(document.querySelector(`.row-${l}`).children).find(item=>item.classList.contains(`col-${rightCross-l}`))); 
+                    for (let l = 1; l < rightCross; l++) {
+                        newArr2.push(Array.from(document.querySelector(`.row-${l}`).children).find(item => item.classList.contains(`col-${rightCross-l}`)));
                     }
                 }
-                if(newArr2.length>=5){
-                    for(let m=0; m<11;m++){
-                        if(newArr2.slice(m,m+5).filter(item=>item.classList.contains('white')).length===5){
-                        finish();
+                if (newArr2.length >= 5) {
+                    for (let m = 0; m < 11; m++) {
+                        if (newArr2.slice(m, m + 5).filter(item => item.classList.contains('white')).length === 5) {
+                            finish();
+                        }
                     }
                 }
-            }
             } else {
                 el.classList.add("black");
-                for(let i=0;i<11;i++){
-                    if(
-                        Array.from(el.closest('.row').children).slice(i,i+5).filter(item=>item.classList.contains('black')).length===5 ||
-                        Array.from(document.querySelectorAll(`.${curCol}`)).slice(i,i+5).filter(item=>item.classList.contains('black')).length===5
-                    ){
+                for (let i = 0; i < 11; i++) {
+                    if (
+                        Array.from(el.closest('.row').children).slice(i, i + 5).filter(item => item.classList.contains('black')).length === 5 ||
+                        Array.from(document.querySelectorAll(`.${curCol}`)).slice(i, i + 5).filter(item => item.classList.contains('black')).length === 5
+                    ) {
                         finish();
                     }
                 }
-                let newArr=[];
-                leftCross=Math.abs(curPo.col-curPo.row);
-                for(let j=15;j>leftCross;j--){
-                    if(curPo.col-curPo.row>=0){
-                    newArr.push(Array.from(document.querySelector(`.row-${j-leftCross}`).children).find(item=>item.classList.contains(`col-${j}`)));
+                let newArr = [];
+                leftCross = Math.abs(curPo.col - curPo.row);
+                for (let j = 15; j > leftCross; j--) {
+                    if (curPo.col - curPo.row >= 0) {
+                        newArr.push(Array.from(document.querySelector(`.row-${j-leftCross}`).children).find(item => item.classList.contains(`col-${j}`)));
+                    } else {
+                        newArr.push(Array.from(document.querySelector(`.row-${j}`).children).find(item => item.classList.contains(`col-${j-leftCross}`)));
+                    }
+                }
+                if (newArr.length >= 5) {
+                    for (let j = 0; j < 11; j++) {
+                        if (newArr.slice(j, j + 5).filter(item => item.classList.contains('black')).length === 5) {
+                            finish();
+                        }
+                    }
+                }
+                let newArr2 = [];
+                rightCross = curPo.col + curPo.row;
+                if (curPo.col + curPo.row > 15) {
+                    for (let k = 15; k > rightCross - 15; k--) {
+                        newArr2.push(Array.from(document.querySelector(`.row-${k}`).children).find(item => item.classList.contains(`col-${rightCross-k}`)));
+                    }
+                    newArr2.push(Array.from(document.querySelector(`.row-${rightCross-15}`).children).find(item => item.classList.contains('col-15')));
                 } else {
-                    newArr.push(Array.from(document.querySelector(`.row-${j}`).children).find(item=>item.classList.contains(`col-${j-leftCross}`)));
-                }          
-                }
-                if(newArr.length>=5){
-                    for(let j=0; j<11;j++){
-                        if(newArr.slice(j,j+5).filter(item=>item.classList.contains('black')).length===5){
-                        finish();
+                    for (let l = 1; l < rightCross; l++) {
+                        newArr2.push(Array.from(document.querySelector(`.row-${l}`).children).find(item => item.classList.contains(`col-${rightCross-l}`)));
                     }
                 }
-            }
-            let newArr2=[];
-            rightCross=curPo.col+curPo.row;
-            console.log(rightCross);
-            if(curPo.col+curPo.row>=15){
-            for(let k=15; k>rightCross-15;k--){  
-            newArr2.push(Array.from(document.querySelector(`.row-${k}`).children).find(item=>item.classList.contains(`col-${rightCross-k}`)));
-            }    
-            newArr2.push(Array.from(document.querySelector(`.row-${rightCross-15}`).children).find(item=>item.classList.contains('col-15')));
-            } else{
-            for(let l=1; l<rightCross;l++){
-            newArr2.push(Array.from(document.querySelector(`.row-${l}`).children).find(item=>item.classList.contains(`col-${rightCross-l}`))); 
+                if (newArr2.length >= 5) {
+                    for (let m = 0; m < 11; m++) {
+                        if (newArr2.slice(m, m + 5).filter(item => item.classList.contains('black')).length === 5) {
+                            finish();
+                        }
+                    }
                 }
+
             }
-            if(newArr2.length>=5){
-                for(let m=0; m<11;m++){
-                    if(newArr2.slice(m,m+5).filter(item=>item.classList.contains('white')).length===5){
-                    finish();
-                }
-            }
-        }
-                
-            }
-                //count 변수 통해 번갈아가며 돌 놓기
-                console.log(Array.from(el.classList)[0].split('-')[1], Array.from(el.closest('.row').classList)[0].split('-')[1]);
-             
+            //count 변수 통해 번갈아가며 돌 놓기
+            console.log(Array.from(el.classList)[0].split('-')[1], Array.from(el.closest('.row').classList)[0].split('-')[1]);
+
         })
     })
 });
